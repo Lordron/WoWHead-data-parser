c9565197d31d46f13980f92c1fa6fafc78da0ef7
Heisenberg <worldofwarcraftcataclysmmail@gmail.com>
2011-09-18 12:04:30 +0300

    Add icon

--- WoWHeadParser/WowHeadParserForm.Designer.cs
+++ WoWHeadParser/WowHeadParserForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WoWHeadParserForm));
             this.parserBox = new System.Windows.Forms.ComboBox();
             this.localeBox = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -171,6 +172,7 @@
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "WoWHeadParserForm";
             this.Text = "WowHead Parser";
             this.groupBox1.ResumeLayout(false);
