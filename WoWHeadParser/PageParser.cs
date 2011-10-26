a6d295defb4ff5b36a7d62c4a81db897e4ccce29
Lordron <gorjitza@mail.ru>
2011-10-26 19:08:30 +0300

    Update WoWHeadParser/PageParser.cs

--- WoWHeadParser/PageParser.cs
+++ WoWHeadParser/PageParser.cs
@@ -10,14 +10,14 @@ namespace WoWHeadParser
         {
             StringBuilder content = new StringBuilder();
             Regex reg = new Regex(@"new Book\({ parent: '.+', pages: \['(?<test>.+)'\]}\)", RegexOptions.Multiline);
-            MatchCollection matches = reg.Matches(block.page);
+            MatchCollection matches = reg.Matches(block.Page);
 
             foreach (Match match in matches)
             {
                 GroupCollection groups = match.Groups;
                 string typeStr = groups["test"].Value ?? string.Empty;
                 string[] pages = typeStr.Split(new[] { @"','" }, StringSplitOptions.RemoveEmptyEntries);
-                content.AppendFormat("SET @ENTRY := SELECT `data0` FROM `gameobject_template` WHERE `entry` = {0};", block.entry).AppendLine();
+                content.AppendFormat("SET @ENTRY := SELECT `data0` FROM `gameobject_template` WHERE `entry` = {0};", block.Entry).AppendLine();
                 content.AppendLine(@"INSERT IGNORE INTO `page_text` (`entry`, `text`, `next_page`) VALUES");
                 for (int i = 0; i < pages.Length; ++i)
                 {
