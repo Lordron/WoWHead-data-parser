8e7a70631ba1aa1d4cdc2e5354fe0d07ab866a05
Heisenberg <worldofwarcraftcataclysmmail@gmail.com>
2012-04-08 14:27:31 +0300

    Missing 2

---
+++ WoWHeadParser/SqlBuilder/SqlItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WoWHeadParser
+{
+    public class SqlItem
+    {
+        private object _key = new object();
+        private List<string> _values = new List<string>(64);
+
+        public SqlItem(object key, List<string> values)
+        {
+            _key = key;
+            _values = values;
+        }
+
+        public string this[int x]
+        {
+            get
+            {
+                if (x >= _values.Count)
+                    throw new IndexOutOfRangeException();
+
+                return _values[x];
+            }
+        }
+
+        /// <summary>
+        /// Gets a key
+        /// </summary>
+        public object Key { get { return _key; } }
+
+        /// <summary>
+        /// Gets the number of elements actually contained in the list
+        /// </summary>
+        public int Count { get { return _values.Count; } }
+    }
+}
